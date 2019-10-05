ed60c64c0eb87d64dd3714fdd7c08cda6203154b
Adri Blanco <adriblabla@gmail.com>
2019-10-05 18:30:40 +0200

    Removed the mock for devices. It avoid to stop the server with a SIGINT signal

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const DeviceManager = require('./devicemanager')
 const WServer = require('./wserver')
-const mockup = require('./mockupdevice')
 const GameController = require('./gameController')
 
 const server = new WServer()
 const gameController = new GameController(server)
 
-// mockup(gameController)
 DeviceManager(gameController)
 
 server.listen(1337)
