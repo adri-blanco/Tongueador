688fd1546914c7d1801266f09a953ae3e05b45ef
Adri Blanco <adriblabla@gmail.com>
2019-10-05 19:15:17 +0200

    Ran prettier to the whole project and fixed eslint problems

--- gameController.js
+++ gameController.js
@@ -1,47 +1,50 @@
-
 function GameController(wsServer) {
-    this.wsServer = wsServer
-    this.scores = {}
-    this.resetting = false
-    this.globalTimeout = null
-    this.winner = null
-    this.timeHit = null
+  this.wsServer = wsServer;
+  this.scores = {};
+  this.resetting = false;
+  this.globalTimeout = null;
+  this.winner = null;
+  this.timeHit = null;
 }
 
-GameController.prototype.onEventPulsed = function(eventCode) {
-    if(this.winner === null) {
-        this.winner = eventCode
-        this.timeHit = new Date();
-        this.onReset()
-        setTimeout( () => {
-            this.winner = null;
-            this.logged = false;
-            console.log('Ready, Sir!\n')
-        }, 10000)
-    } else {
-        if(!this.logged && this.winner !== eventCode) {
-            const difference = new Date() - this.timeHit;
-            this.wsServer.send({ difference });
-            console.log(eventCode, ' hitted ', new Date() - this.timeHit, 'ms late');
-            this.logged = true;
-        }
-    }
-}
+GameController.prototype.onEventPulsed = function onEventPulsed(eventCode) {
+  if (this.winner === null) {
+    this.winner = eventCode;
+    this.timeHit = new Date();
+    this.onReset();
+    setTimeout(() => {
+      this.winner = null;
+      this.logged = false;
+      // eslint-disable-next-line no-console
+      console.log('Ready, Sir!\n');
+    }, 10000);
+  }
+  if (!this.logged && this.winner !== eventCode) {
+    const difference = new Date() - this.timeHit;
+    this.wsServer.send({ difference });
+    // eslint-disable-next-line no-console
+    console.log(eventCode, ' hitted ', new Date() - this.timeHit, 'ms late');
+    this.logged = true;
+  }
+};
 
-GameController.prototype.onDraw = function() {
-    if(this.winner === null) {
-        this.winner = Math.random() > 0.5 ? 'A' : 'B'
-        console.log('Draw, and won: ', this.winner);
-        this.timeHit = new Date();
-        this.onReset()
-        setTimeout( () => { this.winner = null}, 10000)
-    }
-}
+GameController.prototype.onDraw = function onDraw() {
+  if (this.winner === null) {
+    this.winner = Math.random() > 0.5 ? 'A' : 'B';
+    // eslint-disable-next-line no-console
+    console.log('Draw, and won: ', this.winner);
+    this.timeHit = new Date();
+    this.onReset();
+    setTimeout(() => {
+      this.winner = null;
+    }, 10000);
+  }
+};
 
-GameController.prototype.onReset = function() {
-    var winner = this.winner;
-    this.wsServer.send({winner, status:"winner"})
-    this.scores = {}
-}
+GameController.prototype.onReset = function onReset() {
+  const { winner } = this;
+  this.wsServer.send({ winner, status: 'winner' });
+  this.scores = {};
+};
 
-module.exports = GameController
+module.exports = GameController;
--- wserver.js
+++ wserver.js
@@ -1,46 +1,48 @@
 // https://medium.com/@martin.sikora/node-js-websocket-simple-chat-tutorial-2def3a841b61
 
-var WebSocketServer = require("websocket").server;
-var http = require("http");
+import WebSocket from 'websocket';
+import http from 'http';
+
+const WebSocketServer = WebSocket.server;
 
 function WServer() {
   this.connection_pool = [];
 
-  this.server = http.createServer(_ => {
+  this.server = http.createServer(() => {
     // process HTTP request. Since we're writing just WebSockets
     // server we don't have to implement anything.
   });
 
   // create the server
   this.wsServer = new WebSocketServer({
-    httpServer: this.server
+    httpServer: this.server,
   });
 
   // WebSocket server
-  this.wsServer.on("request", (request) => {
-    var connection = request.accept(null, request.origin);
+  this.wsServer.on('request', request => {
+    const connection = request.accept(null, request.origin);
     this.handleConnect(connection);
 
-    connection.on("close", (handler ) => this.handleDisconnect(handler));
+    connection.on('close', handler => this.handleDisconnect(handler));
   });
 }
 
-WServer.prototype.listen = function(port) {
+WServer.prototype.listen = function listen(port) {
   this.server.listen(port, () => {}); // no-op
 };
 
-WServer.prototype.handleConnect = function(connection) {
+WServer.prototype.handleConnect = function handleConnect(connection) {
   this.connection_pool.push(connection);
 };
 
-WServer.prototype.handleDisconnect = function(connection) {
-  var index = this.connection_pool.indexOf(connection);
+WServer.prototype.handleDisconnect = function handleDisconnect(connection) {
+  const index = this.connection_pool.indexOf(connection);
   if (index > -1) {
     this.connection_pool.splice(index, 1);
   }
 };
 
-WServer.prototype.send = function(data) {
+WServer.prototype.send = function send(data) {
   this.connection_pool.forEach(connection =>
     connection.sendUTF(JSON.stringify(data))
   );
