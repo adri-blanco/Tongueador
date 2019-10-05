772e85d96594d09653d4f6325334f9afb85f1e2a
Adri Blanco <adriblabla@gmail.com>
2019-10-05 20:39:39 +0200

    Make the teams dynamic

---
+++ config.js
@@ -0,0 +1,20 @@
+export default {
+  teams: [
+    {
+      teamKey: 'A',
+      dataCode: '0280807f7f0f1000',
+    },
+    {
+      teamKey: 'B',
+      dataCode: '018080007f0f0000',
+    },
+    {
+      teamKey: 'C',
+      dataCode: '0280807f000f0000',
+    },
+    {
+      teamKey: 'D',
+      dataCode: '0180807f7f8f0000',
+    },
+  ],
+};
--- devicemanager.js
+++ devicemanager.js
@@ -1,28 +1,21 @@
 import HID from 'node-hid';
+import GameConfiguration from './config';
 
 const PID = 57345;
 const VID = 2064;
+
+const { teams } = GameConfiguration;
 
 function Devicemanager(gameController) {
   const device = new HID.HID(VID, PID);
   device.on('data', rawData => {
     const data = rawData.toString('hex');
 
-    if (data === '0280807f7f0f1000') {
-      gameController.teamPressedButton('A');
-    }
-
-    if (data === '018080007f0f0000') {
-      gameController.teamPressedButton('B');
-    }
-
-    if (data === '0280807f000f0000') {
-      gameController.teamPressedButton('C');
-    }
-
-    if (data === '0180807f7f8f0000') {
-      gameController.teamPressedButton('D');
-    }
+    teams.forEach(team => {
+      if (data === team.dataCode) {
+        gameController.teamPressedButton(team.teamKey);
+      }
+    });
   });
   // disable-next-line no-console
   device.on('error', console.error);
